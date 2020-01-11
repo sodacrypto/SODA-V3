6fd53f874919faf179a62b019f2b2f872ae1cfce
Darya Gavrilina <gavrilinadar@ya.ru>
2020-01-11 19:38:23 +0800

    localization: open/close/select

--- precompiled/scripts/public/lp.js
+++ precompiled/scripts/public/lp.js
@@ -9,3 +9,20 @@ $("#security-range").ionRangeSlider({
     step: 5,
     prettify: x =>`${x}%`
 })
+
+document.querySelectorAll(".lang-box").forEach(x => x.addEventListener('click', function(){
+	const thisElem = x
+	if (document.querySelectorAll('.overlay-dyn'))
+		document.querySelectorAll('.overlay-dyn').forEach(y => document.body.removeChild(y))
+	const overlay = document.createElement("div")
+	thisElem.appendChild(overlay)
+	overlay.classList.add("overlay-dyn")
+	overlay.addEventListener('click', function(event){
+		thisElem.querySelector(".lang-dropdown").classList.remove("open")
+		thisElem.removeChild(overlay)
+		event.stopPropagation();
+	})
+
+	thisElem.querySelector(".lang-dropdown").classList.add("open")
+
+}))
